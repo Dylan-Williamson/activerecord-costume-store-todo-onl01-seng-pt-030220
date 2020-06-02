5e52bbb6baa5dbc948f0ea2f91aa773789064318
Dylan Williamson <dirtyassweed@icloud.com>
2020-06-02 16:35:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,7 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes, :
+    create_table :costumes do |t|
+      
   end
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
